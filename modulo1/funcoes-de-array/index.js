969426b70149b445be32b26122fb21890778a37b
André <andremarks99@gmail.com>
2021-10-15 22:06:57 +0200

    funcoes-de-array

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -50,18 +50,18 @@
 
 //2-
 
-// const produtos = [
-//     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
-//     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
-//     { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
-//     { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
-//     { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
-//     { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
-//     { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
-//     { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
-//     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
-//     { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
-//  ]
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
 
 // //a)
 // const novoArrayNome = produtos.map((produto) => {
@@ -94,7 +94,7 @@
 
 // console.log(arrayYpe);
 
-// //e)
+//e)
 // const arrayYpe = produtos.filter((produto) => {
 //     return produto.nome.includes("Ypê");
 // });
