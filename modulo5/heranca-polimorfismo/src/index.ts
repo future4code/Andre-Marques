39438348331ad9ae9ff7adc601f16a881323b742
André <andremarks99@gmail.com>
2022-02-16 12:02:12 +0100

    Feito

--- modulo5/heranca-polimorfismo/src/index.ts
+++ modulo5/heranca-polimorfismo/src/index.ts
@@ -1,3 +1,5 @@
+// Heranca
+
 class User {
     private id: string;
     private email: string;
@@ -88,4 +90,113 @@ const fabio = new Customer(
 // console.log(fabio.purchaseTotal)
 
 // 4- 
-console.log(fabio.getIntroduceYourself())
+// console.log(fabio.getIntroduceYourself())
+
+// 5- Feito.
+// console.log(fabio.getIntroduceYourself())
+
+
+//Polimorfismo
+
+export interface Client {
+  name: string;
+  // Refere-se ao nome do cliente
+
+  registrationNumber: number;
+  // Refere-se ao número de cadastro do cliente na concessionária
+	// como se fosse um id
+
+  consumedEnergy: number;
+  // Refere-se à energia consumida pelo cliente no mês
+
+  calculateBill(): number;
+  // Retorna o valor da conta em reais
+}
+
+// 1- a) Todas. Nao. Porque as interfaces sao publicas.
+
+const client: Client = {
+  name: "Goli",
+  registrationNumber: 1,
+  consumedEnergy: 100,
+
+  calculateBill: () => {
+    return 2;
+  }
+}
+
+// console.log(client.name)
+// console.log(client.registrationNumber)
+// console.log(client.consumedEnergy)
+// console.log(client.calculateBill())
+
+// 2 
+export abstract class Place {
+  constructor(protected cep: string) {}
+
+	public getCep(): string {
+		return this.cep;
+  }
+}
+
+// a) Nao se pode instanciar uma classe abstrata.
+// const place = new Place()
+
+// b) para criar uma instância de uma classe abstrata precisamos declarar uma classe filha e criar uma instância dessa última.
+
+// 3- a) para criar uma instância de uma classe abstrata precisamos declarar uma classe filha e criar uma instância dessa última.
+//    b) Place é uma classe porque precisa ter um acesso restrito a um dos seus atributos, o que é impossível de se fazer em interfaces
+//    c) Place é abstrata porque não enxergamos uma situação em que seria necessário criar uma instância dessa classe.
+
+export class Residence extends Place {
+  constructor(
+    protected residentsQuantity: number,
+    // Refere-se ao número de moradores da casa
+
+    cep: string
+  ) {
+    super(cep);
+  }
+
+  public getResidentsQuantity (){
+    return this.residentsQuantity
+  }
+}
+
+export class Commerce extends Place {
+  constructor(
+    protected floorsQuantity: number,
+    // Refere-se à quantidade de andares do lugar
+
+    cep: string
+  ) {
+    super(cep);
+  }
+
+  public getFloorsQuantity (){
+    return this.floorsQuantity
+  }
+}
+
+export class Industry extends Place {
+  constructor(
+    protected machinesQuantity: number, 
+    // Refere-se à quantidade de máquinas do local 
+    
+    cep: string
+		) {
+	    super(cep);
+  }
+
+  public getMachinesQuantity (){
+    return this.machinesQuantity
+  }
+}
+
+const residence:Residence = new Residence(4, "13080080")
+const commerce:Commerce = new Commerce(4, "13080085")
+const industry:Industry = new Industry(4, "13080089")
+
+console.log(residence.getCep())
+console.log(commerce.getCep())
+console.log(industry.getCep())
